349446fd85789c797d4e9a6b36f91ba1306ac515
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2026-03-12 20:48:05 +0100

    Bump flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Minimalstandards der Lehre";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs =
-    inputs@{ self, nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
